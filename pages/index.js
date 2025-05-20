99fed7b33001b9166668b9c186cf10c146c52e9a
Malver <jbalaguerprofesional@gmail.com>
2025-05-20 23:27:17 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ function getWeekNumber(date) {
 }
 
 export default function Home() {
-  const userRole = 'normal';
+  const userRole = 'admin';
 
   const isNormal      = userRole === 'normal';
   const isArcana      = userRole === 'arcana';
